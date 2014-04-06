d9b408d17abea866099b46b5d07d4bcf3ba83c81
netsensei <matthias@colada.be>
2014-04-06 20:58:29 +0200

    Removed: e2e support since this is probably overkill

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,24 +44,7 @@ module.exports = function(grunt) {
         configFile: 'test/karma.conf.js',
         background: false
       }
-    },
-
-    protractor: {
-      options: {
-        configFile: "node_modules/protractor/referenceConf.js", // Default config file
-        keepAlive: true, // If false, the grunt process stops when the test fails.
-        noColor: false, // If true, protractor will not use colors in its output.
-        args: {
-          // Arguments passed to the command
-        }
-      },
-      target: {
-        options: {
-          configFile: "test/protractor.conf"
-        }
-      }
     }
-
   });
 
   grunt.loadNpmTasks('grunt-sass');
@@ -75,5 +58,4 @@ module.exports = function(grunt) {
   grunt.registerTask('buildSass', ['sass:prod', 'cssmin:minify']);
   grunt.registerTask('build', ['buildJs', 'buildSass']);
   grunt.registerTask('test', ['karma']);
-  grunt.registerTask('e2e', ['protractor']);
 };
